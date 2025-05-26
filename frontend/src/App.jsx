63acec6ac08d83cf1ef22e0e7bcd4a2debe94e91
Jexaruuu <barsanajexter@gmail.com>
2025-05-27 03:30:43 +0800

    testing for the payment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,15 +33,15 @@ function App() {
         <Route path="/bookservices" element={<ProtectedRoute element={<BookServices />} />} />
         <Route path="/taskerform" element={<ProtectedRoute element={<TaskerForm />} />} />
         <Route path="/about" element={<About />} />
-        <Route path="/userabout" element={<ProtectedRoute element={<UserAbout/>} />} />
-        <Route path="/adminlogin" element={<AdminLogin/>} />
+        <Route path="/userabout" element={<ProtectedRoute element={<UserAbout />} />} />
+        <Route path="/adminlogin" element={<AdminLogin />} />
         <Route path="/adminsignup" element={<AdminSignup />} />
         <Route path="/admindashboard" element={<AdminDashboard />} />
         <Route path='/editprofile' element={<ProtectedRoute element={<EditProfile />} />} />
         <Route path="/userhome" element={<ProtectedRoute element={<UserHome />} />} />
         <Route path="/userheader" element={<ProtectedRoute element={<UserHeader />} />} />
         <Route path="/clientform" element={<ProtectedRoute element={<ClientForm />} />} />
-        <Route path="/availableworkers" element={<ProtectedRoute element={<AvailableWorkers />} />} />\
+        <Route path="/availableworkers" element={<ProtectedRoute element={<AvailableWorkers />} />} />
         <Route path="/servicerequest" element={<ProtectedRoute element={<UserAvailableServices />} />} />
         <Route path="/payment" element={<ProtectedRoute element={<Payment />} />} /> 
       </Routes>
